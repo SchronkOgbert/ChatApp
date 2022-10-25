76013939b3a2e29343f10d64a710228f0d690159
Daniele Viola <daniele.viola02@e-uvt.ro>
2022-10-25 15:53:58 +0300

    added confirm password field

--- reactapp/src/pages/Register.tsx
+++ reactapp/src/pages/Register.tsx
@@ -24,6 +24,10 @@ const Register = () => {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
             </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control type="password" placeholder="Confirm password" />
+            </Form.Group>
             <Button variant="primary" type="submit">
                 Register
             </Button>
