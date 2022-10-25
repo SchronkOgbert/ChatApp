8c26c133c662ba05f356c581fb31fd563778dd17
Daniele Viola <daniele.viola02@e-uvt.ro>
2022-10-25 12:34:42 +0300

    Register/Login pages done only ui, no functionality

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import Register from './pages/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+      <Container className='mw-25 p-5'>
+      <Routes>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/" element={<Home />}/>
+        <Route path="/register" element={<Register />}/>
+      </Routes>
+      </Container>
+    </>
+    
   );
 }
 
