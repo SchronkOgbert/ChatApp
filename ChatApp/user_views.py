f7e5816124ee658f4c5494d81cc20711b30b2201
Bogdy <bogdan.topliceanu01@e-uvt.ro>
2022-10-25 21:15:43 +0300

    push register route

--- ChatApp/user_views.py
+++ ChatApp/user_views.py
@@ -1,7 +1,11 @@
 import json
 
-from django.contrib import auth
+
+
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.middleware.csrf import get_token
 from django.views.decorators.csrf import csrf_exempt
 
@@ -28,6 +32,36 @@ def login(request):
             "token": None
         }
     return HttpResponse(json.dumps(response))
+
+
+@csrf_exempt
+def register(request):
+    if request.method != 'POST':
+        raise PermissionError("Cannot access this endpoint")
+    data = json.load(request)
+    username = data['username']
+    email = data['email']
+    password = data['password']
+    try:
+        User.objects.create_user(username=username, email=email, password=password)
+        response = {
+            "success": True,
+            "message": "Success",
+            "token": get_token(request)
+        }
+    except Exception as e:
+        response = {
+            "success": False,
+            "message": "Success"
+                    }
+    finally:
+        return HttpResponse(json.dumps(response))
+
+
+
+
+
+
 
 
 
