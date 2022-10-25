ec919ea439af86dbd460ca445ae79cd3a2f6ead7
Kabewh <daniele_viola@yahoo.com>
2022-10-25 14:24:44 +0300

    ignore this

--- reactapp/src/pages/Login.tsx
+++ reactapp/src/pages/Login.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
 
   return (
       <Container className='mw-10'>
         <Form className='m-5 d-flex flex-column align-items-center bg-white shadow p-3 rounded-3 border' >
+          Login
           <Form.Group className="mb-3" controlId="formBasicUsername">
             <Form.Label>Username</Form.Label>
             <Form.Control type="username" placeholder="Enter username" />
@@ -18,6 +20,9 @@ const Login = () => {
           <Button variant="primary" type="submit">
             Login
           </Button>
+          <Form.Text className="text-muted">
+          No account? <Link to="/register">Register here!</Link>
+          </Form.Text>
         </Form>
       </Container>
     
--- reactapp/src/pages/Register.tsx
+++ reactapp/src/pages/Register.tsx
@@ -6,6 +6,7 @@ const Register = () => {
   return (
     <Container className='mw-10'>
         <Form className='m-5 d-flex flex-column align-items-center bg-white shadow p-3 rounded-3 border' >
+            Register
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
@@ -23,6 +24,9 @@ const Register = () => {
             <Button variant="primary" type="submit">
                 Register
             </Button>
+            <Form.Text className="text-muted">
+                Already have an account?<Link to="/login">Login here!</Link>
+            </Form.Text>
         </Form>
     </Container>
   )
