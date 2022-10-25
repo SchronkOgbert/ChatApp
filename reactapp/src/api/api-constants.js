c4818bad5effcca3e1a1f6c922384c83e84bebf6
ralphsz28 <ralph.szakacs01@e-uvt.ro>
2022-10-25 20:40:21 +0300

    .

--- reactapp/src/api/api-constants.js
+++ reactapp/src/api/api-constants.js
@@ -1,4 +1,20 @@
 export const ApiConstants = {
+  loginUrl: "/login",
+
+  httpGet: "GET",
+  httpPost: "POST",
+  httpPut: "PUT",
+  httpDelete: "DELETE",
+
+  httpStatus200: 200,
+  httpStatus500: 500,
+  httpStatus400: 400,
+  httpStatus401: 401,
+  httpStatus403: 403,
+  httpStatus404: 404,
+};
+
+export const ApiConstants = {
   //baseUrl: "127.0.0.1:8000",
   loginUrl: "/login",
 
