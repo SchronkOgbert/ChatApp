872ec679d1e4987b81715f706caf73429710b0de
Damian Sirbu <sirbudamian@gmail.com>
2023-01-08 17:21:44 +0200

    Randomize code generation(for real this time))

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('available/', views.available),
     path('new_code/', views.get_room_code),
     path('find_room/', views.does_room_exist),
     path('<str:room_name>/', views.room, name='room'),  # IMPORTANT: this has to be the last
